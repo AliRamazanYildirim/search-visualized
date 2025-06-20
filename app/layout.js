58632ac31df19ba55f23c939acb72d19976379a4
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2025-06-20 16:29:31 +0200

    feat: ConsoleSignature-Komponente hinzugefügt, um eine visuelle Signatur im Konsolenprotokoll anzuzeigen

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ConsoleSignature from "./components/ConsoleSignature";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,10 +19,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning="true">
+    <html lang="en" suppressHydrationWarning={true}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ConsoleSignature />
         {children}
       </body>
     </html>
